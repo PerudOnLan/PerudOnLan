158ae80388f705dd45c3afc56a547025b1664687
François DE GREEF <francois.de_greef@telecom-sudparis.eu>
2014-04-06 21:05:19 +0200

    Update 0.0.0.1.1
    Maintenant on peut gérer jusqu'à ce qu'un joueur se fasse éliminer.

--- Code/Sandbox/perudo.c
+++ Code/Sandbox/perudo.c
@@ -7,6 +7,7 @@ int main() {
     printf("Combien de joueurs ?\n");
     scanf("%d", &nb_de_joueurs);
     partie(nb_de_joueurs);
+    return EXIT_SUCCESS;
 }
 
 
@@ -22,10 +23,15 @@ void partie(int nb_de_joueurs) {
     while (continuer) {
         int i; int j; int k; int tmp; int joueur_actuel; int nb_de_des_max;
         Booleen tour_de_jeu = VRAI; Annonce annonce_precedente; Annonce annonce;
-        if (premier_tour) { joueur_actuel=0; nb_de_des_max = nb_de_joueurs * 5; }
+        if (premier_tour) {
+            joueur_actuel=0;
+            nb_de_des_max = nb_de_joueurs * 5;
+            for (i=0;i<nb_de_joueurs;i++) {
+                joueurs[i].nb_de_des = 5;
+            }
+        }
         srand(time(NULL));
         for (i=0;i<nb_de_joueurs;i++) {
-            joueurs[i].nb_de_des = 5;
             for (k=0;k<6;k++) { joueurs[i].des[k]=0;}
             for (j=0;j<joueurs[i].nb_de_des;j++) {
                 tmp = rand()%6;
@@ -42,38 +48,100 @@ void partie(int nb_de_joueurs) {
         }
         Booleen premiere_mise = VRAI;
         while (tour_de_jeu) {
-            printf("\nMise du joueur %d ?\n", joueur_actuel%nb_de_joueurs +1);
-            scanf ("%d %d", &annonce.info.mise.nombre, &annonce.info.mise.de);
-            annonce.type = MISE;
+            int m; int e; annonce.type = MISE; int somme = 0;
+            if (!premiere_mise) {
+                printf("\nMenteur ? (0/1)\n");
+                scanf("%d", &m);
+                if (m==1) { annonce.type = MENTEUR; }
+                else if (m==0) {
+                    printf("\nExact ? (0/1)\n");
+                    scanf("%d", &e);
+                    if (e==1) { annonce.type = EXACT; }
+                    else if (e==0) { annonce.type = MISE; }}
+                else { printf("Annonce incorrecte.\n"); }
+            }
             switch (annonce.type) {
             case MISE: {
+                Booleen annonce_incorrecte = VRAI;
+                while (annonce_incorrecte) {
+                printf("\nMise du joueur %d ?\n", joueur_actuel%nb_de_joueurs +1);
+                scanf ("%d %d", &annonce.info.mise.nombre, &annonce.info.mise.de);
                 if (premiere_mise) {
                     if (annonce.info.mise.de > 0 && annonce.info.mise.nombre > 0 && annonce.info.mise.de <7 && annonce.info.mise.nombre < nb_de_des_max) {
                     annonce_precedente.info.mise.nombre = annonce.info.mise.nombre;
                     annonce_precedente.info.mise.de = annonce.info.mise.de;
                     joueur_actuel++;
                     premiere_mise = FAUX;
+                    annonce_incorrecte = FAUX;
                     }
-                    else { printf("Annonce incorrecte."); }
+                    else { printf("Annonce incorrecte.\n"); }
                 }
                 else {
-                if (annonce.info.mise.de > 0 && annonce.info.mise.nombre > 0 && annonce.info.mise.de <7 && annonce.info.mise.nombre < nb_de_des_max
-                    && ((annonce.info.mise.nombre == annonce_precedente.info.mise.nombre && annonce.info.mise.de > annonce_precedente.info.mise.de)
+                if (annonce.info.mise.de > 0 && annonce.info.mise.nombre > 0 && annonce.info.mise.de <7 && annonce.info.mise.nombre <= nb_de_des_max
+                    && ((annonce.info.mise.nombre == annonce_precedente.info.mise.nombre && annonce.info.mise.de > annonce_precedente.info.mise.de && annonce_precedente.info.mise.de != 1)
                     || (annonce.info.mise.nombre > annonce_precedente.info.mise.nombre && annonce.info.mise.de == annonce_precedente.info.mise.de)
                     || (annonce.info.mise.de == 1 && annonce.info.mise.nombre >= (annonce_precedente.info.mise.nombre+1)/2)
                     || (annonce_precedente.info.mise.de == 1 && annonce.info.mise.nombre >= annonce_precedente.info.mise.nombre*2 +1))) {
                     annonce_precedente.info.mise.nombre = annonce.info.mise.nombre;
                     annonce_precedente.info.mise.de = annonce.info.mise.de;
                     joueur_actuel++;
+                    annonce_incorrecte = FAUX;
                 }
-                else { printf("Annonce incorrecte."); }
+                else { printf("Annonce incorrecte.\n"); }
+                }
                 }
                 break;
+            }
+            case MENTEUR: {
+                for (i=0;i<nb_de_joueurs;i++) {
+                    somme += joueurs[i].des[annonce_precedente.info.mise.de -1];
+                }
+                if (somme<annonce_precedente.info.mise.nombre) {
+                    joueurs[(joueur_actuel-1)%nb_de_joueurs].nb_de_des--;
+                    printf("Menteur !\n");
+                    joueur_actuel = (joueur_actuel-1)%nb_de_joueurs;
+                }
+                else {
+                    joueurs[joueur_actuel%nb_de_joueurs].nb_de_des--;
+                    printf("\nEh non, ils y sont !\n");
+                }
+                nb_de_des_max--;
+                tour_de_jeu = FAUX;
+                break;
+            }
+            case EXACT: {
+                for (i=0;i<nb_de_joueurs;i++) {
+                    somme += joueurs[i].des[annonce_precedente.info.mise.de -1];
+                }
+                if (somme==annonce_precedente.info.mise.nombre) {
+                    printf("\nGG, y'a que ca qui marche a ce jeu la.\n");
+                    if (joueurs[joueur_actuel%nb_de_joueurs].nb_de_des <5) {
+                        joueurs[joueur_actuel%nb_de_joueurs].nb_de_des++;
+                        nb_de_des_max++;
+                    }
+                }
+                else {
+                    printf("\nDommage, ce n'est pas exact.\n");
+                    joueurs[joueur_actuel%nb_de_joueurs].nb_de_des--;
+                    nb_de_des_max--;
+                }
+                tour_de_jeu = FAUX;
+                break;
+            }
+            default: {
+                printf("Annonce incorrecte.\n");
             }
 
             }
 
         }
         premier_tour = FAUX;
+        for (i=0;i<nb_de_joueurs;i++) {
+            if (joueurs[i].nb_de_des==0) {
+                continuer = FAUX;
+                printf("Le joueur %d est elimine.", i+1);
+            }
+        }
+        if (continuer) printf("\nNouveau tour de jeu :\n");
     }
 }
