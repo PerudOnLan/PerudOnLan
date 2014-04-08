4f0874009a2608f853411f0eb092c2e808d31746
0megon <Faispaschier@carrouf.fr>
2014-04-08 03:00:05 +0200

    Avancement de l'ia plus test compilation ok. Voir pour les types redondants (menteur et exact -> struct inutile)

--- Code/Sandbox/ia.c
+++ Code/Sandbox/ia.c
@@ -1,25 +1,30 @@
 #include "ia.h"
 
-
+int main()
+{
+return 0;
+}
 
 
 int d5 () {
 
     srand(time(NULL)) ;
-    int res = floor((rand()/RAND_MAX)*5)+1 ;
+    int res = (rand()/RAND_MAX)*5+1 ;
     return res ;
 }
 
 
 
 
-int nombreValeurProbable(int valeur, int nbT, int nbDe, int de[6]) {
+int nombreValeurProbable(int valeur, int nbT, int nbDe, int des[6]) {
 
-    if (valeur != 1)
-        int res = (int)((float)(nbT - nbDe)/3 + 0.5) + de[valeur-1] + de[0] ;
-    else
-        int res = ((int)((float)(nbT - nbDe)/6 + 0.5) + de[0] ;
-
+    int res;
+    if (valeur != 1){
+        res = (int)((float)(nbT - nbDe)/3 + 0.5) + des[valeur-1] + des[0] ;
+    }
+    else{
+        res = (int)((float)(nbT - nbDe)/6 + 0.5) + des[0] ;
+    }
     return res ;
 }
 
@@ -75,20 +80,21 @@ int plusOuMoins (int nbT) {
 
 
 
-void valeurLePlus (int de[6], int *pNombre, int *pValeur, int valeur_precedente) {
+void valeurLePlus (int des[6], int *pNombre, int *pValeur, int valeur_precedente, int nb_de_des) {
 
-    *pNombre = de[1];
+    *pNombre = des[1];
     *pValeur = 2;
+    int i;
     for (i=2;i<6;i++) {
 
-        if (de[i] >= *pNombre) {
-            *pNombre = de[i] + de[0];
+        if (des[i] >= *pNombre) {
+            *pNombre = des[i] + des[0];
             *pValeur = i+1 ;
         }
     }
 
-    if (valeur_precedente > *pNombre || de[0]>= 3)  {
-        *pNombre = de[0];
+    if (valeur_precedente > *pNombre || des[0]>=(nb_de_des/2 +1) )  {
+        *pNombre = des[0];
         *pValeur = 1;
     }
 }
@@ -97,11 +103,15 @@ void valeurLePlus (int de[6], int *pNombre, int *pValeur, int valeur_precedente)
 
 
 
-Annonce cerveauIA (Annonce annonce_precedente, int de[6]) {
+Annonce cerveauIA (Annonce annonce_precedente, int des[6]) {
 
     int nb_de_des_max ;
     Joueur iA ;
     int commentProbable = nombreValeurProbable(annonce_precedente.info.mise.de, nb_de_des_max, iA.nb_de_des, iA.des) - annonce_precedente.info.mise.nombre + plusOuMoins(nb_de_des_max) ;
+    int lancer = d5() ;
+    Annonce annonceIa ;
+    int *pNombre;
+    int *pValeur;
 
     switch (commentProbable) {
 
@@ -111,15 +121,45 @@ Annonce cerveauIA (Annonce annonce_precedente, int de[6]) {
         case 1:
         case 0:
         case -1:
+            switch (lancer) {
+                case 1:
+                    annonceIa.info.mise.de = annonce_precedente.info.mise.de;
+                    annonceIa.info.mise.nombre = annonce_precedente.info.mise.nombre + 1;
+                    return annonceIa;
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    valeurLePlus(iA.des, pNombre, pValeur, annonce_precedente.info.mise.de, nb_de_des_max) ;
+                    if (*pValeur != 1) {
+                        annonceIa.info.mise.nombre = annonce_precedente.info.mise.nombre + 1;
+                        annonceIa.info.mise.de = *pValeur;
+                    }
+                    else {
+                        annonceIa.info.mise.nombre = (1 + annonce_precedente.info.mise.nombre)/2;
+                        annonceIa.info.mise.de = *pValeur;
+                    }
+                    break;
+                case 5:
+                    if (iA.des[annonce_precedente.info.mise.de-1] < annonce_precedente.info.mise.nombre) {
+                        annonceIa.info.exact.exact = VRAI;
+                        break;
+                    }
+                    else {
+                        annonceIa.info.mise.nombre = (1 + annonce_precedente.info.mise.nombre)/2;
+                        annonceIa.info.mise.de = 1;
+                        break;
+                    }
+            }
 
-        case -2:
-        case -3:
+        //case -2:
+       // case -3:
 
     }
 
-    if (commentProbable >= 4) {}
+   // if (commentProbable >= 4) {}
 
-    if (commentProbable <= -4)
+  //  if (commentProbable <= -4) {}
 }
 
 
--- Code/Sandbox/ia.h
+++ Code/Sandbox/ia.h
@@ -10,7 +10,7 @@ int d5 () ;
 int nombreValeurProbable (int valeur, int nbT, int nbDe, int de[6]) ;
 int plusOuMoins (int nbT) ;
 Annonce cerveauIA (Annonce annonce_precedente, int de[6]) ;
-int valeurLePlus (int de[6]) ;
+void valeurLePlus (int de[6], int *pNombre, int *pValeur, int valeur_precedente, int nb_de_des) ;
 
 
 
